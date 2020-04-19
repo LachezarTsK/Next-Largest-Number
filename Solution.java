690bfcf9843189999e68f092c4b2669c7eb48a71
Lachezar Kutsarov <lkutsarov@gmail.com>
2020-04-19 13:22:09 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -9,21 +9,19 @@ public class Solution {
     Scanner scanner = new Scanner(System.in);
     BigInteger inputNumber = scanner.nextBigInteger();
     scanner.close();
-    BigInteger result = nextLargestNumber(inputNumber);
+    
+    BigInteger result = nextLargestInteger(inputNumber);
     System.out.println(result);
-    // int num = 219876311;
-    //   int num = 219876311;
-    // System.out.println(num);
-    //  System.out.println(nextNumber(num));
   }
 
   /**
-   * Finds the next largest integer to the input number by rearanging the digits of it.
+   * Finds the next largest integer to the input integer by rearanging the digits of it.
    *
-   * @return An integer representing the next largest integer to the input number. If it is not
-   *     possible to rearrange the digits for a larger integer, then the input number is returned.
+   * @return An integer representing the next largest integer to the input integer. 
+   *         If it is not possible to rearrange the digits for a larger integer, 
+   *         then the input integer is returned.
    */
-  public static BigInteger nextLargestNumber(BigInteger inputNumber) {
+  public static BigInteger nextLargestInteger(BigInteger inputNumber) {
     num_toString = inputNumber.toString();
 
     int index_leftSwap = findDigit_leastPlaceValue_andNumericValue_lessThanRightNeigbour();
@@ -41,11 +39,12 @@ public class Solution {
    * Finds the digit with least 'place value', which 'numeric value' is also less than the 'numeric
    * value' of its direct neighbour to the right.
    *
-   * <p>Example: 12187465 '4' => digit with least 'place value' and 'numeric value' less than the
-   * 'numeric value' of its direct neighbour to the right.
+   * Example: 12187465 
+   *         '4' => digit with least 'place value' and 'numeric value' less than 
+   *         the 'numeric value' of its direct neighbour to the right.
    *
-   * @return A non-negative integer, representing the index of the found digit. If not such digit is
-   *     found, it returns '-1';
+   * @return A non-negative integer, representing the index of the found digit.
+   *         If not such digit is found, it returns '-1';
    */
   private static int findDigit_leastPlaceValue_andNumericValue_lessThanRightNeigbour() {
     int rightIndex = num_toString.length() - 1;
@@ -62,12 +61,14 @@ public class Solution {
   }
 
   /**
-   * Having steblished the left swap digit, find the most suitable right swap digit.
+   * Having established the left swap digit, find the most suitable right swap digit.
    *
-   * <p>The most suitable swap digit to the right is this digit where: 'left swap digit' < 'digit to
-   * the right' and 'digit to the right' - 'left swap digit' is minimum.
+   * The most suitable swap digit to the right is this digit where: 
+   * 'left swap digit' < 'digit to the right' 
+   *  and 
+   * 'digit to the right' - 'left swap digit' is minimum.
    *
-   * <p>Example: 112651 => leftSwap: 2, (6-2)=4, (5-2)=1, rightSwap: 5.
+   * Example: 112651 => leftSwap: 2, (6-2)=4, (5-2)=1, rightSwap: 5.
    *
    * @return An integer, representing the index of the most suitable swap digit to the right.
    */
@@ -90,10 +91,11 @@ public class Solution {
   }
 
   /**
-   * Interchanges the places of the left and right swap digits. Then sorts in increasing order all
-   * the digits after the swapped digit to the left.
+   * Interchanges the places of the left and right swap digits.
+   * Then sorts in increasing order all the digits after the swapped digit to the left.
    *
-   * <p>Example: 112651 => leftSwap: 2, rightSwap: 5. after swap: 115621, after sort: 115126
+   * Example: 112651 => leftSwap: 2, rightSwap: 5. 
+   *                    after swap: 115621, after sort: 115126
    *
    * @return A string, representing the next largest integer.
    */
@@ -103,14 +105,18 @@ public class Solution {
     result.setCharAt(index_leftSwap, num_toString.charAt(index_rightSwap));
     result.setCharAt(index_rightSwap, num_toString.charAt(index_leftSwap));
 
-    // Store number of occurences of all digits after the swapped digit to the left.
+    /**
+    * Store number of occurences of all digits after the swapped digit to the left.
+    */
     int[] digits = new int[10];
     for (int i = index_leftSwap + 1; i < num_toString.length(); i++) {
       digits[result.codePointAt(i) - ASCII_ZERO]++;
     }
 
-    // Append to result all the digits after after the swapped digit to the left, in increasingn
-    // order.
+    /**
+    * Append to result all the digits after after the swapped digit to the left, 
+    * in increasingn order.
+    */
     int index_sorted = index_leftSwap + 1;
     for (int i = 0; i < 10; i++) {
 
